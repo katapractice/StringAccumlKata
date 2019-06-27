81a603e2f0315c254df53808791330edf7130494
nicholashaynes1 <nicholashaynes1@gmail.com>
2019-06-26 18:52:16 -0600

    My solution to the kata.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,6 +6,32 @@ public class Controller
 	
 	public void start()
 	{
+		 System.out.println(accumulate("abcd"));
+	}
+	
+	
+	private String accumulate(String s)
+	{
+		String returnString = "";
 		
+		int i = 0;
+		while(i < s.length())
+		{
+			char getCharAtIndex = s.charAt(i);
+			int indexChar = s.indexOf(getCharAtIndex);
+			
+			int z = 0;
+			while(z < indexChar + 1)
+			{
+				returnString = returnString.concat(String.valueOf(s.charAt(i)));
+				z++;
+			}
+			returnString.concat("-");
+			
+			
+			i++;
+		}
+		
+		return returnString;
 	}
 }
